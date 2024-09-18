8f5cd48fffa570ef42961f4d80bd8671da2c3532
mariia-23 <m.roghozhina@mail.ru>
2024-09-18 15:40:18 +0300

    add second and third tasks to HW_4

---
+++ HW_4/Task_3.js
@@ -0,0 +1,14 @@
+const minAge = 18;
+const maxAge = 60;
+let age = parseInt("11");
+if(typeof age !== "number") {
+    console.log("Incorrect data type");  
+} else if(age < minAge) {
+    console.log("You don't have access cause your age is " + age + "\nIt's less than " + minAge);    
+} else if (age >= minAge && age < maxAge) {
+    console.log("Welcome!");
+} else if (age > maxAge) {
+    console.log("Keep calm and look Culture channel");    
+} else {
+    console.log("Technical work");    
+}
